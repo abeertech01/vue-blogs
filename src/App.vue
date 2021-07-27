734df1c3bf662cc7ac90b707b04490a834eb438e
Abeer <abeer.technology@gmail.com>
2021-07-27 18:05:59 +0600

    made the header ui for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-header />
+    <router-view />
   </div>
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+export default {
+  name: "App",
+  components: {
+    AppHeader: Header,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./sass/variables";
+body {
+  font-family: $reem-kufi;
+  color: $charcoal;
 }
 
-#nav {
-  padding: 30px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+a,
+a:active,
+a:hover {
+  text-decoration: none;
+  color: $charcoal;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="header-inside">
+      <div class="logo">
+        <router-link :to="{ name: 'Home' }">VueBlogs</router-link>
+      </div>
+      <div class="menu">
+        <i class="bx bx-menu"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../sass/variables";
+.header {
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  &-inside {
+    display: flex;
+    justify-content: space-between;
+    padding: pr(5) pr(10);
+
+    .logo {
+      a {
+        font-size: pr(25);
+        font-weight: 600;
+      }
+    }
+
+    .menu {
+      font-size: pr(30);
+    }
+  }
+}
+</style>
